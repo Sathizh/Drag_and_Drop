c09948b9f42a0aa99ecc287770f3d4c43c60650b
Sathizh <sathizh20@gmail.com>
2022-05-21 08:30:23 +0530

    responsive message added

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -24,55 +24,58 @@ const Sidebar = () => {
 
     return (
         <div className="flex w-full">
-            <div className={` ${open ? "lg:w-60 md:w-52" : "lg:w-20 md:w-16 w-16"} bg-indigo-900 text-white h-screen pt-6 relative duration-300 shadow-md `}>
-                <div className={`absolute cursor-pointer -right-3 inset-y-1/2 h-6 w-6  flex items-center justify-center bg-white text-black hover:bg-purple-500 hover:text-black rounded-full  ${!open && "rotate-180"} shadow-md`}
-                    onClick={() => setOpen(!open)}>
-                    <ion-icon name="chevron-back-outline"></ion-icon>
-                </div>
-                <div className="flex gap-x-4 items-center justify-center">
-                    <div >
-                    <ion-icon name="aperture-outline" class="h-8 w-8 hover:bg-gray-100/20 rounded-full p-2">
-                </ion-icon>
+            <div className="hidden lg:flex ">
+                <div className={` ${open ? "lg:w-60 md:w-52" : "lg:w-20 md:w-16 w-16"} bg-indigo-900 text-white h-screen pt-6 relative duration-300 shadow-md `}>
+                    <div className={`absolute cursor-pointer -right-3 inset-y-1/2 h-6 w-6  flex items-center justify-center bg-white text-black hover:bg-purple-500 hover:text-black rounded-full  ${!open && "rotate-180"} shadow-md`}
+                        onClick={() => setOpen(!open)}>
+                        <ion-icon name="chevron-back-outline"></ion-icon>
+                    </div>
+                    <div className="flex gap-x-4 items-center justify-center">
+                        <div >
+                        <ion-icon name="aperture-outline" class="h-8 w-8 hover:bg-gray-100/20 rounded-full p-2">
+                    </ion-icon>
+                        </div>
+                    </div>
+                    <div className="px-2 mt-10 h-5/6 flex flex-col justify-between gap-1  ">
+                        <div>
+                            {TopMenus.map((Menu, index) => (
+                                <NavLink to={`${Menu.url}`}
+                                    key={index}
+                                    className={ style.navLink}
+                                >
+                                    <ion-icon name={Menu.icon} class="h-6 w-6"></ion-icon>
+                                    <div className="flex flex-row flex-none gap-1">
+                                        <span className={`${!open && "hidden"} origin-left duration-200 capitalize truncate`}>
+                                            {Menu.title}
+                                        </span>
+                                    </div>
+                                </NavLink>
+                            ))}
+                        </div>
+                        <div>
+                            {BottomMenus.map((Menu, index) => (
+                                <NavLink to={`${Menu.url}`}
+                                    key={index}
+                                    className={style.navLink}
+                                >
+                                    <ion-icon name={Menu.icon} class="h-6 w-6"></ion-icon>
+                                    <div className="flex flex-row flex-none gap-1">
+                                        <span className={`${!open && "hidden"} origin-left duration-200 capitalize truncate`}>
+                                            {Menu.title}
+                                        </span>
+                                    </div>
+                                </NavLink>
+                            ))}
+                        </div>
                     </div>
                 </div>
-                <div className="px-2 mt-10 h-5/6 flex flex-col justify-between gap-1  ">
-                    <div>
-                        {TopMenus.map((Menu, index) => (
-                            <NavLink to={`${Menu.url}`}
-                                key={index}
-                                className={ style.navLink}
-                            >
-                                <ion-icon name={Menu.icon} class="h-6 w-6"></ion-icon>
-                                <div className="flex flex-row flex-none gap-1">
-                                    <span className={`${!open && "hidden"} origin-left duration-200 capitalize truncate`}>
-                                        {Menu.title}
-                                    </span>
-                                </div>
-                            </NavLink>
-                        ))}
-                    </div>
-                    <div>
-                        {BottomMenus.map((Menu, index) => (
-                            <NavLink to={`${Menu.url}`}
-                                key={index}
-                                className={style.navLink}
-                            >
-                                <ion-icon name={Menu.icon} class="h-6 w-6"></ion-icon>
-                                <div className="flex flex-row flex-none gap-1">
-                                    <span className={`${!open && "hidden"} origin-left duration-200 capitalize truncate`}>
-                                        {Menu.title}
-                                    </span>
-                                </div>
-                            </NavLink>
-                        ))}
+                <div className="flex-1 bg-gray-100 overflow-x-hidden">
+                    <div className="w-full h-full">
+                        <Outlet />
                     </div>
                 </div>
             </div>
-            <div className="flex-1 bg-gray-100 overflow-x-hidden">
-                <div className="w-full h-full">
-                    <Outlet />
-                </div>
-            </div>
+            <div class="lg:hidden flex justify-center items-center flex-col h-screen text-lg text-center text-white capitalize font-bold bg-gradient-to-t from-indigo-500 to-red-500">Please Try this in Desktop for Better Experiance... <span class="text-3xl">😥</span></div>
         </div>
     );
 };
